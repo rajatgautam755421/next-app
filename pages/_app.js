4d6e495e7d5eb795a843ac22679f12615eb19f15
rajatgautam755421 <rg755421@gmail.com>
2022-01-30 12:16:38 +0545

    sdjkgsd

--- pages/_app.js
+++ pages/_app.js
@@ -37,13 +37,13 @@ function MyApp({ Component, pageProps }) {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
       </Head>
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       />
 
       <ChakraProvider>
